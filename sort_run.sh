f1aa5e57bebfa44930149a263c14c5fb883434c2
Pely Tsintari <50149290+Pely@users.noreply.github.com>
2026-01-20 13:51:28 -0500

    Update loop range in sort_run.sh script

--- sort_run.sh
+++ sort_run.sh
@@ -3,24 +3,14 @@ reglom=0
 
 #make clean && make
 #cd ..
-for i in 2158
+for i in {2165..2168}
 do
+	
 	bash create_rawROOT.sh "$i" "$current" "$reglom"
-
+	
 	bash create_sortROOT.sh "$i" "$current"
+	
 	#runNum="$i"
-	#hadd -f /mnt/analysis/e20008/rootfiles/total_run0${runNum}_10us.root /mnt/analysis/e20008/rootfiles/run0${runNum}-*.roo
-  
- 	#cd root_scripts
- 	#root -l "DSSD_gain_match.C(""$i"")"
- 	#cd ../
-  
-	#cd secarAnalysis
-	#python3 run_secarAnalysis_neut.py "$i"
-	#cd ../
- 
-  #cd Kr_an_2025/acceptance_transmission/detector_yields
-  #root -l -b -q "integrateSpectra.C(""$i"")"
-  #cd ../../../
+	#hadd -f /mnt/analysis/e20008/rootfiles/total_run0${runNum}_10us.root /mnt/analysis/e20008/rootfiles/run0${runNum}-*.root
 
 done
