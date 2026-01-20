a58e428b76b89dcd5500f514341ba127a99a9716
Pely Tsintari <50149290+Pely@users.noreply.github.com>
2026-01-20 13:51:51 -0500

    Update DAQROOT path and uncomment thisroot.sh

--- environment.sh
+++ environment.sh
@@ -1,9 +1,7 @@
 source /usr/opt/buster.sh
 
 #source /mnt/misc/sw/x86_64/Debian/10/root/gnu/6.18.00/bin/thisroot.sh
-#source /mnt/misc/sw/x86_64/Debian/10/root/gnu/6.24.02/bin/thisroot.sh
 source /usr/opt/root/root-6.24.06/bin/thisroot.sh
 
-#export DAQROOT=/usr/opt/nscldaq/11.3-022 
 export DAQROOT=/usr/opt/nscldaq/12.0-021
 export DAQLIB=$DAQROOT/lib
